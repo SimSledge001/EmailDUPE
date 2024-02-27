6a8fb2612bcb5d6c2bb5c614d3a5a3545307f5e4
Simeon Sledge <sledge.simeon@gmail.com>
2024-02-27 18:31:14 -0500

    Initial commit

---
+++ app/src/main/java/com/example/emaildupe/Email.kt
@@ -0,0 +1,10 @@
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.emaildupe.R
+
+class Email(
+    val sender: String,
+    val title: String,
+    val summary: String) {
+}
